c53d744e022b44856d85bf950c08626f5478d404
Jonathan Niles <jonathanwniles@gmail.com>
2015-12-07 12:54:34 +0100

    CHORE(repo): cosmetic enhancements to the codebase

--- config.js
+++ config.js
@@ -1,21 +1,15 @@
 module.exports = {
-  'emails' : [
-  {
+  'emails' : [{
    'name' : 'daily',
-    //'frequency' : '*/1 * * * *', //(for test) 
-    'frequency' : '0 19 * * *', 
-    'addressList' : 'test'
-  }, 
-  {
+    'frequency' : '0 19 * * *',
+    'addressList' : 'da'
+  }, {
     'name' : 'weekly',
-    // 'frequency' : '*/1 * * * *', //(for test) 
     'frequency' : '0 20 * * 0',
-    'addressList' : 'test'
-  }, 
-  {
+    'addressList' : 'wk'
+  }, {
     'name' : 'monthly',
-     // 'frequency' : '*/1 * * * *', //(for test) 
     'frequency' : '0 21 28-31 * *',
-    'addressList' : 'test'
+    'addressList' : 'mn'
   }]
 };
